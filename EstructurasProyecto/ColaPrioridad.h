77a5446917708fb492a931cd95bf340c9aabdf62
pgat3000 <pgat3000@gmail.com>
2020-09-24 12:00:12 -0600

    agregagando archivos de trabajo

---
+++ EstructurasProyecto/ColaPrioridad.h
@@ -0,0 +1,38 @@
+#ifndef COLAPRIORIDAD_H
+#define COLAPRIORIDAD_H
+
+#define CAPACIDAD_MIN	5
+
+// Cola de prioridad de máximos - almacena únicamente números enteros
+// Valor mayor tiene mayor prioridad
+class ColaPrioridad {
+public:
+	ColaPrioridad() : capacidad{ CAPACIDAD_MIN }, tam{ 0 } { cola = new int[capacidad]; }
+	ColaPrioridad(int);
+	ColaPrioridad(ColaPrioridad&);
+	~ColaPrioridad();
+
+	void Agregar(int);
+	int GetMax();
+	int ExtraerMax();
+	void IncrementarLlave(int, int);
+
+
+
+private:
+	// Estructura para almacenar los elementos de la cola
+	int* cola;
+	int capacidad;
+	int tam;
+
+	// Funciones auxiliares
+	int GetPadre(int);
+	int GetHijoIzquierdo(int);
+	int GetHijoDerecho(int);
+	void Swap(int, int);
+	void MaxHeapify(int);
+	void SiftUp(int);
+	void ReasignarMemoria();
+};
+
+#endif
